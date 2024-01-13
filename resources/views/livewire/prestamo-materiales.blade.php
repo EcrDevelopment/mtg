2260866acef6a6e1a49f9cfbcadaeb83bea7d67a
GustavoSistema <gf5535125@gmail.com>
2024-01-13 10:44:48 -0500

    prest

--- resources/views/livewire/prestamo-materiales.blade.php
+++ resources/views/livewire/prestamo-materiales.blade.php
@@ -40,7 +40,7 @@
                             @livewire('agregar-articulo-prestamo',["disponibles"=>$disponibles], key($disponibles->count()))            
                         @endif
                     </div>
-                    {{var_export($seleccionados)}}
+                    {{--{{var_export($seleccionados)}}--}}
                     <x-jet-input-error for="articulos" />
                     <div class="m-4">
                         @if (count($articulos))
